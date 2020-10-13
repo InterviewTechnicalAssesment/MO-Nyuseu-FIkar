549cb0c7dfc9f0025227f48746f14effb17b0eb3
Muhammad Zulfikar <mzulfikar@id.ibm.com>
2020-10-13 15:35:44 +0700

    change category to card view

---
+++ nyuseu/app-entity/CategoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryModel.swift
+//  nyuseu
+//
+//  Created by Muhammad Zulfikar on 13/10/20.
+//
+
+import Foundation
+import UIKit
+
+struct Category {
+    let name: String
+    let image: UIImage
+    let nameColor: UIColor
+}
--- nyuseu/app-module/category-module/CategoryViewController.swift
+++ nyuseu/app-module/category-module/CategoryViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     var presenter: ViewToPresenterCategoryProtocol?
-    @IBOutlet weak var categoryTableView: UITableView!
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
     
-    var categoryArrayList = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    var categoryArrayList: [Category] = [
+        Category(name: "business", image: #imageLiteral(resourceName: "business"), nameColor: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)),
+        Category(name: "entertainment", image: #imageLiteral(resourceName: "entertainment"), nameColor: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)),
+        Category(name: "general", image: #imageLiteral(resourceName: "general"), nameColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
+        Category(name: "health", image: #imageLiteral(resourceName: "health"), nameColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
+        Category(name: "science", image: #imageLiteral(resourceName: "science"), nameColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
+        Category(name: "sports", image: #imageLiteral(resourceName: "sports"), nameColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)),
+        Category(name: "technology", image: #imageLiteral(resourceName: "technology"), nameColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setupNavigationBar()
-        setupCategoryTableView()
+        setupCategoryCollectionView()
     }
     
     func setupNavigationBar() {
@@ -27,33 +35,46 @@ class CategoryViewController: UIViewController {
         navigationItem.title = "News Category"
     }
     
-    func setupCategoryTableView() {
-        categoryTableView.dataSource = self
-        categoryTableView.delegate = self
+    func setupCategoryCollectionView(){
+        categoryCollectionView.dataSource = self
+        categoryCollectionView.delegate = self
     }
 
 }
 
-extension CategoryViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return categoryArrayList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryViewCell", for: indexPath)
-        cell.textLabel?.text = categoryArrayList[indexPath.row].capitalized
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionCell", for: indexPath) as! CategoryCollectionViewCell
+        cell.categoryName.text = categoryArrayList[indexPath.row].name.capitalized
+        cell.categoryName.textColor = categoryArrayList[indexPath.row].nameColor
+        cell.categoryImage.image = categoryArrayList[indexPath.row].image
         return cell
     }
     
     
 }
 
-extension CategoryViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        presenter?.startNavigateToSource(navigationController: navigationController!, category: categoryArrayList[indexPath.row])
+extension CategoryViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        presenter?.startNavigateToSource(navigationController: navigationController!, category: categoryArrayList[indexPath.row].name)
     }
 }
 
 extension CategoryViewController: PresenterToViewCategoryProtocol {
     
+}
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        categoryImage.layer.cornerRadius = 20
+    }
 }
